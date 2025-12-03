5c1242f8c8e302034858471c8e9d5ba3e7cee2c5
Hunsoo0oo <118656829+Hunsoo0oo@users.noreply.github.com>
2025-12-03 18:31:10 +0700

    Add files via upload

---
+++ client.py
@@ -0,0 +1,91 @@
+﻿import requests
+import json
+from pydantic import BaseModel
+from typing import Union
+#from os.path import json
+import time
+import random
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = "Описание..."
+    price: float
+    id: Union[int, None] = -1
+
+    def __str__(self):
+        return f"{self.name}, {self.description} за {self.price} рублей"
+
+
+def send_get(url):
+    headers = {'Authorization': 'xxx'}
+    response = requests.get(url, headers=headers) 
+    return response.text, response.status_code
+
+
+
+def reg_user():
+    email = input("Email:")
+    login = input("Логин:")
+    password = input("Пароль:")
+    password_confirmation = input("Подтвердите пароль:")
+    if password == password_confirmation:  
+        user = {"login": login, "email": email, "password": password}
+        response = requests.post("http://127.0.0.1:8000/users/", json=user)
+        if response.status_code == 200:
+            print("Пользователь зарегистрирован")
+        else:
+            print("Ошибка регистрации")
+    else:
+        print("Пароли не совпадают")
+
+
+def auth_user():
+    login = input("Введите логин:")
+    password = input("Введите пароль:")
+    user = {"login": login, "password": password}
+    response = requests.post("http://127.0.0.1:8000/users/auth", json=user)
+    if response.status_code == 200:
+        data = response.json()
+        Token = data["Token"]
+        print("Авторизация прошла успешно")
+    else:
+        print("Неверный логин или пароль")
+
+    
+
+def all_items():
+    result, code = send_get("http://localhost:8000/items")
+    match code:
+        case 200:
+            json_items = json.loads(result)
+            for json_item in json_items:
+                item =  Item(**json_item)
+                if (item.description != None):
+                    print(item)
+        case 401:
+            print("Неверные авторизационные данные")
+        case 403:
+            print("Доступ ограничен")
+        case _:
+            print("Неизвестная ошибка")
+    
+
+
+while (True):
+    try:
+        print("Введите команду")
+        command = int(input("1 - cписок товаров\n2 - добавить товар\n3 - регистрация\n4 - авторизация\n"))
+        match command:
+            case 1:
+                all_items()
+            case 2:
+                create_item()
+            case 3:
+                reg_user()
+            case 4:
+                auth_user()
+            case _:
+                break
+    except ValueError:
+        print("Неверно")
---
+++ main.py
@@ -0,0 +1,89 @@
+﻿from typing import Union
+from fastapi import FastAPI, HTTPException, Request
+from pydantic import BaseModel
+import json
+import time
+from os import listdir
+from os.path import isfile, join 
+import os
+import random
+
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = "Описание..."
+    price: float
+    id: Union[int, None] = -1
+
+
+class User(BaseModel):
+    login: str
+    email: str
+    password: str
+    role: Union[str, None] = "basic role"
+    token: Union[str, None] = None
+    id: Union[int, None] = -1
+
+
+class AuthUser(BaseModel):
+    login: str
+    password: str
+
+
+@app.get("/")
+def root_path():
+    return {"Hello": "World", "code": 744, "names":[{"name": "ivan", "surname": "ivanov",}]}
+
+
+
+@app.get("/items/{item_id}")
+def item_read(item_id: int, q: Union[str, None] = 0, a: Union[str, None] = 0 ):
+    sum = q +a
+    return {"item_id": item_id, "q": q, "a": a, "sum": sum}
+
+
+@app.post("/items/")
+def create_item(item: Item):
+   item.id = int(time.time())
+
+   with open(f"items/item_{item.id}.json", "w") as f:
+       json.dump(item.model_dump(), f)
+   return item
+
+
+@app.get("/items")
+def all_times(request: Request):
+    token = request.headers.get('Authorization')
+    if token != 'xxx':
+        raise HTTPException(status_code=401, detail= "Invalid token")
+    json_files_names = [file for file in os.listdir("items/") if file.endswith(".json")]
+    data = []
+    for json_file_name in json_files_names:
+        file_path = os.path.join("items/", json_file_name)
+        with open(file_path, "r") as f:
+            data.append(json.load(f))
+    return data
+
+
+@app.post("/users/")
+def create_user(user: User):
+   user.id = int(time.time())
+   user.token = str(random.getrandbits(128))
+
+   with open(f"users/user_{user.id}.json", "w") as f:
+       json.dump(user.model_dump(), f)
+   return user
+
+
+@app.post("/users/auth")
+def create_user(params: AuthUser):
+    json_files_names = [file for file in os.listdir("users/") if file.endswith(".json")]
+    for json_file_name in json_files_names:
+        file_path = os.path.join("users/", json_file_name)
+        with open(file_path, "r") as f:
+            json_item = json.load(f)
+            user = User(**json_item)
+            if user.login == params.login and user.password == params.password:
+                return {"Login": user.login, "Token": user.token}
