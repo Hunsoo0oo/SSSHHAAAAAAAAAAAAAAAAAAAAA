83d99d43131fc32cc0859ab413cea25e919da551
Hunsoo0oo <palagin_artem.com@mail.ru>
2026-01-28 21:12:03 +0700

    доб. 2я и 3я подпись

---
+++ client.py
@@ -0,0 +1,139 @@
+﻿from tkinter import CURRENT
+import requests
+import json
+from pydantic import BaseModel
+from typing import Union
+#from os.path import json
+import time
+import random
+import re
+import hashlib
+CURRENT_TOKEN = None
+
+
+
+def send_get(url):
+    headers = {'Authorization': hashtoken({})}
+    response = requests.get(url, headers=headers) 
+    return response.text, response.status_code
+
+
+def hashtoken1():
+    global CURRENT_TOKEN
+    if CURRENT_TOKEN is None:
+        return ""
+    sha256_hash = hashlib.new('sha256')
+    sha256_hash.update(CURRENT_TOKEN.encode('utf-8'))
+    return sha256_hash.hexdigest()
+
+
+def hashtoken2():
+    global CURRENT_TOKEN
+    if CURRENT_TOKEN is None:
+        return ""
+    tt = str(int(time.time()))
+    sha256_hash = hashlib.new('sha256')
+    sha256_hash.update((CURRENT_TOKEN + tt).encode('utf-8'))
+    return sha256_hash.hexdigest()
+
+
+def hashtoken(body):
+    global CURRENT_TOKEN
+    if CURRENT_TOKEN is None:
+        return ""
+    body_json = json.dumps(body, separators=(",", ":"), sort_keys=True)
+    tokenbody= CURRENT_TOKEN + body_json
+    return hashlib.sha256(tokenbody.encode()).hexdigest()
+
+
+def reg_user():
+    email = input("Email:")
+    login = input("Логин:")
+    password = pass_check()
+    user = {"login": login, "email": email, "password": password}
+    response = requests.post("http://127.0.0.1:8000/users/", json=user)
+    if response.status_code == 200:
+        print("Пользователь зарегистрирован")
+    elif response.status_code == 409:
+        print("Логин уже занят")
+    else:
+        print("Ошибка регистрации")
+
+
+def auth_user():
+    global CURRENT_TOKEN
+    login = input("Введите логин:")
+    password = input("Введите пароль:")
+    user = {"login": login, "password": password}
+    response = requests.post("http://127.0.0.1:8000/users/auth", json=user)
+    if response.status_code == 200:
+        data = response.json()
+        CURRENT_TOKEN = data["Token"]
+        print("Авторизация прошла успешно",CURRENT_TOKEN)
+    elif response.status_code == 401:
+        print("Неверный логин или пароль")
+    else:
+        print(f" Ошибка {response.status_code}")
+
+
+def pass_check():
+    while (True):
+        password = input("Пароль:")
+        password_confirmation = input("Подтвердите пароль:")
+    
+        if password != password_confirmation: 
+            print("Пароли не совпадают")
+            continue
+
+        if len(password) < 10:
+            print("Пароль должен содержать не менее 10 символов")
+            continue
+    
+        if not re.search(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#_])', password):
+            print("Пароль должен содержать:")
+            print("Cтрочные буквы (a–z)")
+            print("Заглавные буквы (A–Z)")
+            print("Цифры (0–9)")
+            print("Спецсимволы: @$!%*?&#_")
+            continue
+    
+        print("Пароль принят!")
+        return password
+
+
+def shifratbash():
+    global CURRENT_TOKEN
+    if CURRENT_TOKEN is None:
+        return "Авторизируйтесь"
+
+    text = input("Введите текст для шифрования Атбаш: ")
+    body = {"text": text}
+    headers = {'Authorization': hashtoken(body)}
+
+    response = requests.post("http://127.0.0.1:8000/atbash", json=body, headers=headers)
+    
+    if response.status_code == 200:
+        result = response.json()["result"]
+        print(f"Результат шифрования: {result}")
+    elif response.status_code == 401:
+        print("Ошибка подписи! Неверный токен или подпись.")
+    else:
+        print(f"Ошибка сервера: {response.status_code}")
+
+
+
+while (True):
+    try:
+        print("Введите команду")
+        command = int(input("1 - авторизация\n2 - регистрация\n3 - Шифр Атбаш\n"))
+        match command:
+            case 1:
+                auth_user()
+            case 2:
+                reg_user()
+            case 3:
+                shifratbash()
+            case _:
+                break
+    except ValueError:
+        print("Неверно")
---
+++ main.py
@@ -0,0 +1,122 @@
+﻿from typing import Union
+from fastapi import FastAPI, HTTPException, Request
+from pydantic import BaseModel
+import json
+import time
+from os import listdir
+from os.path import isfile, join 
+import os
+import random
+import hashlib
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = "Описание..."
+    price: float
+    id: Union[int, None] = -1
+
+
+class User(BaseModel):
+    login: str
+    email: str
+    password: str
+    role: Union[str, None] = "basic role"
+    token: Union[str, None] = None
+    id: Union[int, None] = -1
+
+
+class AuthUser(BaseModel):
+    login: str
+    password: str
+
+class AtbashRequest(BaseModel):
+    text: str
+
+
+@app.get("/")
+def root_path():
+    return {"Hello": "World", "code": 744, "names":[{"name": "ivan", "surname": "ivanov"}]}
+
+
+@app.post("/atbash")
+async def atbash_endpoint(request: Request):
+    hashtoken = request.headers.get('Authorization')
+    body = await request.json()
+    
+    if not session(hashtoken, body):
+        raise HTTPException(status_code=401, detail="Invalid signature")
+    
+    text = body.get("text", "")
+
+    rus_alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+    eng_alphabet = "abcdefghijklmnopqrstuvwxyz"
+    
+    result = []
+    for char in text:
+        if char.lower() in rus_alphabet:
+            alphabet = rus_alphabet
+            minion = char.lower()
+            oldi = alphabet.index(minion)
+            newi = (len(alphabet) - 1) - oldi
+            new_char = alphabet[newi]
+            result.append(new_char if char.islower() else new_char.upper())
+        elif char.lower() in eng_alphabet:
+            alphabet = eng_alphabet
+            minion = char.lower()
+            oldi = alphabet.index(minion)
+            newi = (len(alphabet) - 1) - oldi
+            new_char = alphabet[newi]
+            result.append(new_char if char.islower() else new_char.upper())
+        else:
+            result.append(char)
+    
+    return {"result": ''.join(result)}
+
+
+@app.post("/users/")
+def create_user(user: User):
+   json_files_names = [file for file in os.listdir("users/") if file.endswith(".json")]
+   for json_file_name in json_files_names:
+        file_path = os.path.join("users/", json_file_name)
+        with open(file_path, "r") as f:
+                openfiles = json.load(f)
+                if openfiles.get("login") == user.login:
+                    raise HTTPException(status_code=409, detail="Такой логин уже есть!")
+   user.id = int(time.time())
+   user.token = str(random.getrandbits(128))
+
+   with open(f"users/user_{user.id}.json", "w") as f:
+       json.dump(user.model_dump(), f)
+   return user
+
+
+@app.post("/users/auth")
+def auth_user(params: AuthUser):
+    json_files_names = [file for file in os.listdir("users/") if file.endswith(".json")]
+    for json_file_name in json_files_names:
+        file_path = os.path.join("users/", json_file_name)
+        with open(file_path, "r") as f:
+            json_item = json.load(f)
+            user = User(**json_item)
+            if user.login == params.login and user.password == params.password:
+                return {"Login": user.login, "Token": user.token}
+    raise HTTPException(status_code=401, detail="Неправльный логин или пароль")
+
+
+def session(hashtoken: str,body: dict):
+    json_files_names = [file for file in os.listdir("users/") if file.endswith(".json")]
+    for json_file_name in json_files_names:
+        file_path = os.path.join("users/", json_file_name)
+        with open(file_path, "r") as f:
+            json_item = json.load(f)
+            token = json_item.get("token")
+            if token:
+                body_json = json.dumps(body, separators=(",", ":"), sort_keys=True)
+                data = token + body_json
+                hashcheck = hashlib.sha256(data.encode('utf-8')).hexdigest()
+                if hashcheck == hashtoken:
+                    return True
+    return False
+
